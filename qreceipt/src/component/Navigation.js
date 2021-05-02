14f9d702a95416233bcd3a02a1429bcc2f2ff20a
Lee Hyo Jin <director20844@gmail.com>
2021-05-03 01:27:37 +0900

    21/05/03 1:27 메뉴바 수정한 거 병합
    따로 작업하던 메뉴바 코드 병합
    클릭 했을 때 반응이 없는 문제 존재, 확인해야함

--- qreceipt/src/component/Navigation.js
+++ qreceipt/src/component/Navigation.js
@@ -1,16 +1,116 @@
 import React from "react";
 import './Navigation.css';
 
+var menu = document.querySelectorAll('.menu, .menu *');
+var menubars = document.querySelectorAll('.menubars, .menubars *');
+var uls = document.querySelectorAll('.menu ul')
+
+function openMenu() {
+    document
+        .querySelector('.menubackground')
+        .style
+        .left = '0px';
+    document
+        .querySelector('.menubackground')
+        .style
+        .top = '-810px';
+    document
+        .querySelector('.top')
+        .style
+        .top = '10px';
+    document
+        .querySelector('.bottom')
+        .style
+        .top = '10px';
+
+    for (var i = 0; i < uls.length; i++) {
+        uls[i].visibility = 'visible';
+    }
+
+    document
+        .querySelector('.top')
+        .style
+        .transform = 'rotate(45deg)';
+    document
+        .querySelector('.bottom')
+        .style
+        .transform = 'rotate(-45deg)';
+    document
+        .querySelector('.middle')
+        .style
+        .transform = 'rotate(45deg)';
+}
+
+function closeMenu() {
+    for (var i = 0; i < uls.length; i++) {
+        uls[i].visibility = 'hidden';
+        uls[i].display = 'none';
+    }
+
+    document
+        .querySelector('.top')
+        .style
+        .top = '0px';
+    document
+        .querySelector('.bottom')
+        .style
+        .top = '20px';
+    document
+        .querySelector('.top')
+        .style
+        .transform = 'rotate(0deg)';
+    document
+        .querySelector('.bottom')
+        .style
+        .transform = 'rotate(0deg)';
+    document
+        .querySelector('.middle')
+        .style
+        .transform = 'rotate(0deg)';
+    document
+        .querySelector('.menubackground')
+        .style
+        .left = '-2240px';
+    document
+        .querySelector('.menubackground')
+        .style
+        .top = '-2240px';
+}
+
 function Navigation() {
     return (
-        <nav className="navbar navbar-expand-lg fixed-top p-0 w-100">
-            <div className="collapse navbar-collapse flex-grow-0 ml-auto">
-                <ul className="navbar-nav p-0">
-                    <li className="nav-item Item" >영수증 모아보기</li>
-                    <li className="nav-item Item">회원가입</li>
+        <div onLoad={()=>{
+            var visable = "";
+            for (var i = 0; i < menubars.length; i++) {
+                menubars[i].addEventListener('click', () => {
+                    for (var j = 0; j < menu.length; j++) {
+                        visable = uls[j].visibility;
+                        if ((visable === "visible") || (visable === "")) {
+                            closeMenu();
+                        } else {
+                            openMenu();
+                        }
+                    }
+                }, false);
+            }
+        }
+        }>
+            <div className="menu">
+                <div className="menubars">
+                    <div className="menubar top"></div>
+                    <div className="menubar middle"></div>
+                    <div className="menubar bottom"></div>
+                </div>
+                <div className="menubackground"></div>
+                <ul className="menulinks">
+                    <li className="item">영수증 모아보기</li>
+                    <li className="item">회원가입</li>
+                    <li className="item">영수증 어쩌고</li>
+                    <li className="item">저쩌고</li>
                 </ul>
             </div>
-        </nav>
+        </div>
+
     )
 }
 
